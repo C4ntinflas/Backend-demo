77dffa5a8f1afda3c42b6f27dad6080d04a609b0
Jonathan <jonathanrodriguez069@gmail.com>
2023-12-19 13:18:40 -0500

    Backend

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+const personRoutes = require('./Routes/Person');
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/person', personRoutes);
+
+// DB connection
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('DB connected'))
+    .catch(err => console.error(err));
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
